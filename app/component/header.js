e9d02385f5dfa19530c13ecb6a1c7ec3491183c6
Rakesh kumar <Rakeshkr479@gmail.com>
2023-07-02 00:06:09 +0530

    4th

--- app/component/header.js
+++ app/component/header.js
@@ -36,12 +36,8 @@ function Header(){
 						<div className="col-md-6">
 							<div className="header-search">
 								<form>
-									<select className="input-select">
-										<option value="0">All Categories</option>
-										<option value="1">Category 01</option>
-										<option value="1">Category 02</option>
-									</select>
-									<input className="input" placeholder="Search here" />
+									
+									<input className="input" placeholder="Search here" style={{borderRadius: 26px 0px 0px 26px;}} />
 									<button className="search-btn">Search</button>
 								</form>
 							</div>
